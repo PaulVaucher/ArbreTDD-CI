9af70ddda0ea729ac6ad1b164d82affc9cf90699
Paul Vaucher <araghast@gmail.com>
2024-11-25 16:43:31 +0100

    add package-lock

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,5 +3,9 @@ const assert = require('node:assert');
 const { fizzbuzz } = require('./fizzbuzz.js')
 
 test('fizzbuzz(3)', (t) => {
-  assert.strictEqual(fizzbuzz(3), "Fizz");
+    assert.strictEqual(fizzbuzz(3), "Fizz");
+});
+
+test('fizzbuzz(15)', (t) => {
+    assert.strictEqual(fizzbuzz(15), "Fizzbuzz");
 });
